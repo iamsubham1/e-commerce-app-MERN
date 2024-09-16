ada05662a0e68a04d001c673c136ef3c7f040032
subham das <das1998lipun@gmail.com>
2024-09-16 23:41:09 +0530

    final changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -59,7 +59,7 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
         const token = generateJWT(req.user);
         res.cookie('JWT', token, { httpOnly: false, maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: 'None', secure: true });
         const encodedToken = encodeURIComponent(token);
-        const redirectUrl = `https://e-commerce-app-mern-bmty.onrender.com/googleloginnextstep?token=${encodedToken}`;
+        const redirectUrl = `https://gadgetsgrabapp.netlify.app/googleloginnextstep?token=${encodedToken}`;
 
         // Redirect the user to the constructed URL
         res.redirect(redirectUrl);
